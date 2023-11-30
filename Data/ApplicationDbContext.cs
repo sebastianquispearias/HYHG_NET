cdaf9080594a05fa41174567de6ed43bb2dd1b07
sebastianquispearias <sebastianquispe1000367@gmail.com>
2023-11-30 18:44:45 -0500

    Primer commit del proyecto CORRECTO30NOV

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CORRECTO30NOV.Models;
+
+namespace CORRECTO30NOV.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CORRECTO30NOV.Models.Joke>? Joke { get; set; }
+    }
+}
